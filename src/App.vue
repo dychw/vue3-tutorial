49563bdeda2c1244efaf99a754d219cb347800c6
dychw <25680873+dychw@users.noreply.github.com>
2020-12-16 15:25:41 +0900

    Add DC Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <template>
-  <h1>Hey</h1>
-  <p>...</p>
+  <AppHeader />
+  <div class="w-full flex">
+    <DcHeros />
+  </div>
 </template>
+
+<script>
+import AppHeader from "./components/AppHeader";
+import DcHeros from "./components/DcHeros";
+export default {
+  components: { AppHeader, DcHeros },
+};
+</script>
+
+<style></style>
