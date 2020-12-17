c620662287ff76c987d4da3659469509358c4c9f
dychw <25680873+dychw@users.noreply.github.com>
2020-12-17 11:50:33 +0900

    Add markdown component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,13 +2,28 @@
   <nav
     class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2"
   >
-    <router-link class="mx-2" to="/dc-heros">DC Heroes</router-link>
-    <router-link class="mx-2" to="/calendar">Calendar</router-link>
+    <router-link
+      v-for="item in list"
+      class="mx-2"
+      :key="item.to"
+      :to="item.to"
+      >{{ item.title }}</router-link
+    >
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      list: [
+        { title: "DC Heroes", to: "/dc-heros" },
+        { title: "Calendar", to: "/calendar" },
+        { title: "Markdown", to: "/markdown" },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
