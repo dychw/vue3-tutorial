61ab12894b326507d56c1aa39f870c2250400fde
dychw <25680873+dychw@users.noreply.github.com>
2020-12-23 18:56:27 +0900

    Add video streaming function

--- src/pages/Tensorflow.vue
+++ src/pages/Tensorflow.vue
@@ -3,6 +3,25 @@
     <div class="m-auto">
       <div class="mt-10">
         <h1 class="text-2xl my-4">Tensorflow Object Detection</h1>
+        <div class="flex flex-wrap justify-center m-2">
+          <div class="w-full flex justify-center">
+            <button
+              v-if="!isStreaming"
+              @click="openCamera"
+              class="w-32 rounded shadow-md bg-gradient-to-r from-red-800 to-pink-600 text-white px-2 py-1"
+            >
+              Open Camera
+            </button>
+            <button
+              v-else
+              @click="stopStreaming"
+              class="w-32 rounded shadow-md bg-gradient-to-r from-red-800 to-pink-600 text-white px-2 py-1"
+            >
+              Stop Streaming
+            </button>
+          </div>
+          <video ref="videoRef" autoplay="true" width="100"></video>
+        </div>
         <div class="flex flex-wrap justify-center">
           <img ref="imgRef" src="../p20lite.png" width="200" />
           <div class="w-full text-center my-4">
@@ -31,7 +50,9 @@ const cocoSsd = require("@tensorflow-models/coco-ssd");
 export default {
   setup() {
     const imgRef = ref("");
+    const videoRef = ref("");
     const isLoading = ref(false);
+    const isStreaming = ref(false);
     const result = ref([]);
 
     async function detect() {
@@ -43,7 +64,36 @@ export default {
       isLoading.value = false;
     }
 
-    return { imgRef, result, detect, isLoading };
+    async function openCamera() {
+      if (navigator.mediaDevices.getUserMedia) {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cams = devices.filter((device) => device.kind === "videoinput");
+        const camId = cams[0].deviceId;
+        navigator.mediaDevices
+          .getUserMedia({ video: { deviceId: { exact: camId } } })
+          .then((stream) => {
+            isStreaming.value = true;
+            videoRef.value.srcObject = stream;
+          });
+      }
+    }
+
+    function stopStreaming() {
+      const stream = videoRef.value.srcObject;
+      const tracks = stream.getTracks();
+      tracks.map((track) => track.stop());
+    }
+
+    return {
+      imgRef,
+      result,
+      detect,
+      isLoading,
+      openCamera,
+      videoRef,
+      isStreaming,
+      stopStreaming,
+    };
   },
 };
 </script>
